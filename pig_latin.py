69a243b8db60a4873f9bd70abe75fce33067ad51
Alex Cooper <AlexCooper@Alexs-MBP.home>
2018-08-29 22:26:43 -0400

    Add docstring and changed else statement to lowercase the first char

--- pig_latin.py
+++ pig_latin.py
@@ -1,9 +1,12 @@
 def pig_latin(word):
+    '''
+    Function to convert string to piglatin
+    '''
     first_letter = word[0]
         # check if first letter is a vowel
     if first_letter in 'aeiou':
         pig_word = word + 'ay'
     else:
-        pig_word = word[1:] + first_letter + 'ay'
+        pig_word = word[1:] + first_letter.lower() + 'ay'
         
     return pig_word
